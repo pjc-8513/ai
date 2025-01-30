b90950ead7f992f1dffa92f36d837713e728fc8e
pjc-8513 <159651772+pjc-8513@users.noreply.github.com>
2025-01-30 14:50:09 +0000

    no more worker

--- public/App (copy).js
+++ public/App (copy).js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import InputArea from './InputArea';
-import OutputArea from './OutputArea';
+import OutputArea from '../src/OutputArea';
 import LoadingSpinner from './LoadingSpinner';
 import './styles.css';
 
