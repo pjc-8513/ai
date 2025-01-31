2bfe2d75c87352212605ae0ee2bdcfe584e98863
pjc-8513 <159651772+pjc-8513@users.noreply.github.com>
2025-01-31 20:44:47 +0000

    added split

--- api/splitCsv.js
+++ api/splitCsv.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { promisify } from 'util';
 import { v4 as uuidv4 } from 'uuid';
+import serveStatic from 'serve-static';
 
 export const config = {
     api: {
@@ -17,11 +18,23 @@ if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+const serve = serveStatic(uploadDir, { index: false });
+
 export default async function handler(req, res) {
+    if (req.method === 'GET' && req.url.startsWith('/tmp/uploads/')) {
+        try {
+            return serve(req, res, () => {
+              res.statusCode = 404
+              res.end('file not found');
+            });
+         } catch(err){
+           return res.status(500).json({ error: 'Internal server error - getting file' });
+         }
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
-
     try {
         const form = formidable({ multiples: false });
         const [fields, files] = await new Promise((resolve, reject) => {
